456c1a171bd33d8562739d216070b6165b8acb41
Rosaura <rosauracarrascomontalvan@gmail.com>
2025-04-15 21:20:44 -0500

    Update server.js
    fix cambios

--- src/server.js
+++ src/server.js
@@ -35,5 +35,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor levantado en http://localhost:${PORT}`);
 });
